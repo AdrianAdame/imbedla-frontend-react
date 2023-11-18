bb0beffc3ea25fbdeede3d9ee8584d9fcc5bbfbf
edgarlandal <mlanda@uabc.edu.mx>
2023-11-18 11:35:20 -0800

    Iniit Project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+import { Button } from "@material-tailwind/react";
+function App() {
+
+  return (
+    <>
+
+      <Button>Hello</Button>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
+
+
